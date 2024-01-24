ceed5d3b65d4a72b1426fc8ce1ea92aaf792a839
Pulfer Dadson <just_pulfer@Trudys-iMac.gateway.sasktel.net>
2024-01-24 05:46:18 -0800

    added functionality to change advice on click....everything seems to be working

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-// Fetch API data using Promises and Async/Await
+// Targeting elements in the DOM
+const adviceId = document.querySelector(".advice-id");
+const adviceText = document.querySelector(".advice-text");
+const btn = document.getElementById("btn");
+
+// Fetching API data using Promises and Async/Await
+// We first declare the api link
 const apiLink = "https://api.adviceslip.com/advice";
 
-const adviceGeneratorObj = async () => {
+// write an async function to retrieve api data
+const apiRetriever = async () => {
 	const res = await fetch(apiLink);
-	const adviceObj = await res.json();
-	return adviceObj;
+	const apiJson = await res.json();
+	return apiJson;
 };
 
-adviceGeneratorObj();
+// Generate the api slip object that is returned to us from the apiRetriever function
+const apiGenerator = async () => {
+	const apiData = await apiRetriever();
+	const apiSlip = apiData.slip;
 
-// const advice = async () => {
-// 	const { id, advice } = adviceGeneratorObj;
-// };
+	// we call Render Advice and pass in the apiSlip
+	renderAdvice(apiSlip);
+};
+
+// function to render advice to the component
+const renderAdvice = (adviceObj) => {
+	const { id, advice } = adviceObj;
+	adviceId.textContent = `ADVICE #${id}`;
+	adviceText.textContent = advice;
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+	btn.addEventListener("click", apiGenerator);
+});
